717fa1d60cf2387b82cd56220ee0e60531f86db6
Isaac Estrella <32748903+Kyptdin@users.noreply.github.com>
2024-05-09 20:11:34 -0400

    Added some function for the google login

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -5,26 +5,28 @@ import { Button } from "@/components/ui/button";
 import { SearchBar } from "@/components/SearchBar";
 
 import { Link } from "react-router-dom";
+import { loginWithGoogle } from "@/services/auth";
 
-export const Navbar = ({showNavbar = true} : {
-  showNavbar?: boolean
-}) => {
-  return (<div className="px-5 py-[30px] mx-[25px] flex justify-between items-center font-outfit">
-    <Link 
-      to={"/"}
-      className="flex items-center justify-center text-gray-200"
-    >
-      <ImLibrary size={25}/>
-      <h2 className="font-bold ml-3">COMP SCI HIGH LIBRARY</h2>
-    </Link>
+export const Navbar = ({ showNavbar = true }: { showNavbar?: boolean }) => {
+  return (
+    <div className="px-5 py-[30px] mx-[25px] flex justify-between items-center font-outfit">
+      <Link to={"/"} className="flex items-center justify-center text-gray-200">
+        <ImLibrary size={25} />
+        <h2 className="font-bold ml-3">COMP SCI HIGH LIBRARY</h2>
+      </Link>
 
-    {showNavbar ? <SearchBar/> : <></>}
+      {showNavbar ? <SearchBar /> : <></>}
 
-    <div>
-      <Button variant="secondary" className="rounded-full">
-        <BsGoogle size={20} className="mr-3"/>
-        LOGIN WITH GOOGLE
-      </Button>
+      <div>
+        <Button
+          variant="secondary"
+          className="rounded-full"
+          onClick={() => loginWithGoogle()}
+        >
+          <BsGoogle size={20} className="mr-3" />
+          LOGIN WITH GOOGLE
+        </Button>
+      </div>
     </div>
-  </div>);
-}
+  );
+};
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { HomePage } from "./pages/HomePage.tsx";
 import { Search } from "./pages/Search.tsx";
+import React from "react";
 
 const router = createBrowserRouter([
   {
@@ -19,7 +19,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-  <RouterProvider router={router} />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
