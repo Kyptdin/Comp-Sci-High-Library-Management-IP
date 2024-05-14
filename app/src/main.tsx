ec25b45d2267b29bb205f636bc712b53b53ef96e
Isaac Estrella <32748903+Kyptdin@users.noreply.github.com>
2024-05-14 14:55:20 -0400

    Added some auth routes

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -10,6 +10,7 @@ import React from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ProtectedPage } from "./pages/ProtectedPage.tsx";
+import { Toaster } from "@/components/ui/toaster";
 
 const queryClient = new QueryClient();
 const router = createBrowserRouter([
@@ -48,6 +49,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
+      <Toaster />
     </QueryClientProvider>
   </React.StrictMode>
 );
--- app/src/pages/ProtectedPage.tsx
+++ app/src/pages/ProtectedPage.tsx
@@ -1,16 +1,17 @@
 import { useGetLoggedInUser } from "@/hooks/useGetLoggedInUser";
 import { ReactNode } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
   children: ReactNode;
 }
 
 export const ProtectedPage = ({ children }: Props) => {
+  const navigate = useNavigate();
   const { data: userData } = useGetLoggedInUser();
 
   if (!userData) {
-    return <Link to={"notAuthorized"} />;
+    navigate("notAuthorized");
   }
 
   return <>{children}</>;
