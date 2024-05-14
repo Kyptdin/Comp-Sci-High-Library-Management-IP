dcf78716098370f393659934d8a39d19a5d6cb4c
Isaac Estrella <32748903+Kyptdin@users.noreply.github.com>
2024-05-14 15:09:32 -0400

    Added a loading indicator

--- app/src/pages/SearchPage.tsx
+++ app/src/pages/SearchPage.tsx
@@ -4,7 +4,7 @@ import { Error } from "@/components/Error";
 import { BookDisplaySkeleton } from "@/components/BookDisplaySkeleton";
 
 import { useParams } from "react-router-dom";
-// @ts-ignore comment
+// @ts-expect-error comment
 import { fetcher } from "@/hooks/fetcher";
 import useSWR from "swr";
 
@@ -14,39 +14,54 @@ export const SearchPage = () => {
   const { searchQuery } = useParams();
 
   const filteredSearchParams = searchQuery
-    ?.split(" ").join("+")
+    ?.split(" ")
+    .join("+")
     .toLocaleLowerCase();
 
   const fetchingUrl = openLibraryUrl + filteredSearchParams;
   const { error, isLoading, data } = useSWR(fetchingUrl, fetcher);
+  const numberOfBooks = isLoading
+    ? "loading"
+    : !data?.numFound
+    ? "No"
+    : data?.numFound;
 
-  return (<div className="min-h-screen bg-gradient-to-t from-gray-950 to-teal-950">
-      <Navbar/>
+  return (
+    <div className="min-h-screen bg-gradient-to-t from-gray-950 to-teal-950">
+      <Navbar />
 
       <p className="text-3xl text-white font-outfit my-3 ml-[40px]">
-        {data?.numFound || "No"} results found for "{searchQuery}"
+        {numberOfBooks} results found for "{searchQuery}"
       </p>
 
-      {error ? <Error/> : <></>}
-      {isLoading ? <BookDisplaySkeleton/> : <></>}
+      {error ? <Error /> : <></>}
+      {isLoading ? <BookDisplaySkeleton /> : <></>}
 
       <div className="grid grid-cols-5 gap-1 p-5">
-        {data ? data.docs.map((bookData : any, key: number) => {
-          let { title, author_name, isbn } = bookData;
+        {data ? (
+          data.docs.map((bookData: any, key: number) => {
+            let { title, author_name, isbn } = bookData;
 
-          author_name = author_name ? author_name[0] : "Unknown";
-          const imageLoaded = isbn ? 
-            `https://covers.openlibrary.org/b/isbn/${isbn[0]}-L.jpg` : 
-            "/blank_book.jpg";
+            author_name = author_name ? author_name[0] : "Unknown";
+            const imageLoaded = isbn
+              ? `https://covers.openlibrary.org/b/isbn/${isbn[0]}-L.jpg`
+              : "/blank_book.jpg";
 
-          return (<BookDisplay
-            author={author_name}
-            isAvaliable={true}
-            image={imageLoaded}
-            key={key}
-          >{title}</BookDisplay>);
-        }) : <></>}
-      </div> 
+            return (
+              <BookDisplay
+                author={author_name}
+                isAvaliable={true}
+                image={imageLoaded}
+                key={key}
+              >
+                {title}
+              </BookDisplay>
+            );
+          })
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 };
