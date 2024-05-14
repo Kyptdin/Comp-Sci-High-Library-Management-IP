9f56380f1e39242ec2a0e815c2e122c8f53702f7
Isaac Estrella <32748903+Kyptdin@users.noreply.github.com>
2024-05-14 15:52:16 -0400

    Made the logo bigger

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -11,9 +11,12 @@ export const Navbar = ({ showNavbar = true }: { showNavbar?: boolean }) => {
 
   return (
     <div className="px-5 py-[30px] mx-[25px] flex justify-between items-center font-outfit">
-      <Link to={"/"} className="flex items-center justify-center text-gray-200">
-        <ImLibrary size={25} />
-        <h2 className="font-bold ml-3">COMP SCI HIGH LIBRARY</h2>
+      <Link
+        to={"/"}
+        className="flex items-center justify-center text-gray-200 "
+      >
+        <ImLibrary size={30} />
+        <h2 className="font-bold ml-3 text-lg">COMP SCI HIGH LIBRARY</h2>
       </Link>
 
       {showNavbar ? <SearchBar /> : null}
