057a10b9e67ac091b52a1fa051468e332fea49c0
Isaac Estrella <32748903+Kyptdin@users.noreply.github.com>
2024-05-15 17:17:34 -0400

    Created the appearnece of the input eleents

--- app/src/subpages/AddBooksAdminPage.tsx
+++ app/src/subpages/AddBooksAdminPage.tsx
@@ -1,11 +1,24 @@
 import { cn } from "@/lib/utils";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export const AddBooksAdminPage = () => {
+  const [bookIsbn, setBookIsbn] = useState<string>("");
+  const [totalCopies, setTotalCopies] = useState<number>(0);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert("Submitting");
+  };
+
   return (
     <div className="p-4 w-[80%]">
       <h2 className="text-xl mb-2 text-white">Add Book</h2>
-      <form className="flex flex-col gap-4 w-1/3">
-        <input
+      <form className="flex flex-col gap-4 w-1/3" onSubmit={onSubmit}>
+        <Input
+          value={bookIsbn}
+          onChange={(e) => setBookIsbn(e.target.value)}
           type="text"
           placeholder="Book isbn"
           className={cn(
@@ -14,7 +27,9 @@ export const AddBooksAdminPage = () => {
             "duration-50 transition ease-out"
           )}
         />
-        <input
+        <Input
+          onChange={(e) => setTotalCopies(parseInt(e.target.value))}
+          value={totalCopies}
           type="number"
           placeholder="Total amount of copies"
           className={cn(
@@ -23,6 +38,9 @@ export const AddBooksAdminPage = () => {
             "duration-50 transition ease-out"
           )}
         />
+        <Button className="bg-teal-700 hover:bg-teal-800 transition duration-200">
+          Hi
+        </Button>
       </form>
     </div>
   );
