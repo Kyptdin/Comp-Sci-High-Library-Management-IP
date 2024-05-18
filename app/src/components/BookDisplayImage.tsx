ef47fb6f4195c4c5491c11959ac7a3a29edb8f46
Md Safi Momen <Momen10462@gmail.com>
2024-05-18 18:38:47 -0400

    Display the image when searching students

--- app/src/components/BookDisplayImage.tsx
+++ app/src/components/BookDisplayImage.tsx
@@ -1,10 +1,14 @@
 import { cn } from "@/lib/utils";
 
-export const BookDisplayImage = ({ src } : {src: string}) => {
+export const BookDisplayImage = ({ src, className } : {
+    src: string
+    className?: string
+}) => {
     return <div className={cn(
         "w-[325px] h-[500px] m-4 relative",
         "bg-black overflow-clip rounded-3xl font-outfit shadow-lg shadow-gray-900",
-        "flex flex-col justify-end items-center"
+        "flex flex-col justify-end items-center",
+        className
     )}>
         <img src={src} className="w-full h-full absolute fill-gray-400"/>
 
--- app/src/components/BorrowBookDisplay.tsx
+++ app/src/components/BorrowBookDisplay.tsx
@@ -1,6 +1,7 @@
 import { Borrow } from "@/types/supabaseTypes"
 
 import { BookDisplaySkeleton } from "@/components/BookDisplaySkeleton";
+import { BookDisplayImage } from "@/components/BookDisplayImage";
 
 import useSWR from 'swr'
 import { getIsbnLink } from "@/utils/isbnApi";
@@ -8,16 +9,22 @@ import { getIsbnLink } from "@/utils/isbnApi";
 import { fetcher } from "@/hooks/fetcher";
 
 export const BorrowBookDisplay = ({bookData} : {bookData: Borrow}) => {
-    // const { data, error, isLoading } = useSWR(getIsbnLink(bookData.isbn), fetcher)
+    const { data, error, isLoading } = useSWR(getIsbnLink(bookData.isbn), fetcher)
+
+    const firstBookItem = data?.items[0];
+    const info = firstBookItem?.volumeInfo;
 
     return <div className="justify-start flex mb-2">
-        {/* {isLoading ? 
-        <BookDisplaySkeleton className="p-0 m-0 h-[220px] w-[150px] rounded-md"/> :
-        <></>} */}
+        {isLoading | error ? 
+        <BookDisplaySkeleton className="p-0 mr-5 h-[220px] w-[150px] rounded-md"/> :
+        <BookDisplayImage 
+            src={info?.imageLinks?.smallThumbnail}
+            className="p-0 mr-5 h-[220px] w-[150px] rounded-md"
+        />}
 
-        <div className="py-7 flex flex-col justify-around">
+        {!isLoading ? <div className="py-7 flex flex-col justify-around">
             <div className="text-white mb-5">
-                <h1 className="text-2xl">Book title</h1>
+                <h1 className="text-2xl">{info?.title}</h1>
                 <h2 className="text-white mb-2">ISBN: {bookData?.isbn}</h2>
                 <h2 className="text-xl">Due date: {bookData?.return_due_date}</h2>
             </div>
@@ -25,11 +32,14 @@ export const BorrowBookDisplay = ({bookData} : {bookData: Borrow}) => {
             <div className="mb-5">
                 <p className="mb-2">Current status:</p>
                 <div className="text-xs inline-flex flex-wrap gap-2">
-                    <p className="px-4 py-1 bg-orange-700 rounded-full h-fit">BORROWED</p>
+                    {/* TODO: Add the missing tag by comparing current date with due date */}
                     <p className="px-4 py-1 bg-red-800 rounded-full h-fit">MISSING</p>
-                    <p className="px-4 py-1 bg-green-800 rounded-full h-fit">RETURNED</p>
+
+                    {bookData.returned ?
+                    <p className="px-4 py-1 bg-green-800 rounded-full h-fit">RETURNED</p> :
+                    <p className="px-4 py-1 bg-orange-700 rounded-full h-fit">BORROWED</p>}
                 </div>
             </div>
-        </div>
+        </div> : <></>}
     </div>
 }
