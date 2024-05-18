27e2766e67be39a6c3a91b6c341170e3e59c7b51
Md Safi Momen <38478828+SafiMomen@users.noreply.github.com>
2024-05-18 13:43:48 -0400

    Code transfer from laptop to PC

--- app/src/components/BookDisplaySkeleton.tsx
+++ app/src/components/BookDisplaySkeleton.tsx
@@ -2,14 +2,15 @@ import { cn } from "@/lib/utils";
 
 import { Skeleton } from "@/components/ui/skeleton";
 
-export const BookDisplaySkeleton = () => {
+export const BookDisplaySkeleton = ({className}: {className?: string}) => {
   return (
     <div className="p-5">
       <Skeleton
         className={cn(
           "bg-gradient-to-t from-teal-950 to-teal-900 opacity-20",
           "w-[325px] h-[500px] m-4 relative",
-          "rounded-3xl relative shadow-lg shadow-gray-900"
+          "rounded-3xl relative shadow-lg shadow-gray-900",
+          className
         )}
       />
     </div>
--- app/src/subpages/SearchStudentAdminPage.tsx
+++ app/src/subpages/SearchStudentAdminPage.tsx
@@ -1,43 +1,107 @@
 import { Input } from "@/components/ui/input";
 
+import { BookDisplaySkeleton } from "@/components/BookDisplaySkeleton";
+
 export const SearchStudentAdminPage = () => {
   return (
-    <div className="w-[80%] p-4">
+    <div className="w-[80%] p-4 font-outfit">
       <div className=" p-4 rounded-md">
+
         <Input
           type="text"
           placeholder="Search"
           className="w-full px-4 py-2 mb-4 rounded "
         />
+
         <div className="text-white">
-          <h2 className="text-xl mb-2">John Doe</h2>
-          <div className="flex gap-3 mb-4">
-            <p className="">Missing: 2</p>
-            <p className="">Borrowed: 6</p>
-            <p className="">Returned: 4</p>
+          <div className="my-[40px] p-2">
+            <h2 className="text-3xl mb-5">Name: John Doe</h2>
+
+            <div className="flex text-lg text-gray-700">
+              <p className="p-1 px-4 mx-1 rounded-full bg-white">
+                Missing: <span className="font-bold text-black">2</span>
+              </p>
+              <p className="p-1 px-4 mx-1 rounded-full bg-white">
+                Borrowed: <span className="font-bold text-black">6</span>
+              </p>
+              <p className="p-1 px-4 mx-1 rounded-full bg-white">
+                Returned: <span className="font-bold text-black">4</span>
+              </p>
+            </div>
           </div>
+
           <div className="grid grid-cols-4 gap-4">
-            <div className="bg-gray-800 p-4 rounded">
-              <div className="w-full h-32 bg-gray-500 rounded mb-2"></div>
-              <p className="text-center">BORROWED</p>
-              <p className="text-center">MISSING</p>
+            <div className="p-2 rounded-[35px]">
+              <BookDisplaySkeleton className="w-full p-0 m-0"/>
+
+              <div className="px-5 gap-2 py-2 pb-5 text-xs flex flex-wrap">
+                <p className="px-4 py-1 bg-orange-700 rounded-full">BORROWED</p>
+                <p className="px-4 py-1 bg-red-800 rounded-full">MISSING</p>
+                <p className="px-4 py-1 bg-green-800 rounded-full">RETURNED</p>
+              </div>
             </div>
-            <div className="bg-gray-800 p-4 rounded">
-              <div className="w-full h-32 bg-gray-500 rounded mb-2"></div>
-              <p className="text-center">BORROWED</p>
-              <p className="text-center">RETURNED</p>
+
+            <div className="p-2 rounded-[35px]">
+              <BookDisplaySkeleton className="w-full p-0 m-0"/>
+
+              <div className="px-5 gap-2 py-2 pb-5 text-xs flex flex-wrap">
+                <p className="px-4 py-1 bg-orange-700 rounded-full">BORROWED</p>
+                <p className="px-4 py-1 bg-red-800 rounded-full">MISSING</p>
+                <p className="px-4 py-1 bg-green-800 rounded-full">RETURNED</p>
+              </div>
             </div>
-            <div className="bg-gray-800 p-4 rounded">
-              <div className="w-full h-32 bg-gray-500 rounded mb-2"></div>
-              <p className="text-center">BORROWED</p>
-              <p className="text-center">BORROWING</p>
+
+            <div className="p-2 rounded-[35px]">
+              <BookDisplaySkeleton className="w-full p-0 m-0"/>
+
+              <div className="px-5 gap-2 py-2 pb-5 text-xs flex flex-wrap">
+                <p className="px-4 py-1 bg-orange-700 rounded-full">BORROWED</p>
+                <p className="px-4 py-1 bg-red-800 rounded-full">MISSING</p>
+                <p className="px-4 py-1 bg-green-800 rounded-full">RETURNED</p>
+              </div>
             </div>
-            <div className="bg-gray-800 p-4 rounded">
-              <div className="w-full h-32 bg-gray-500 rounded mb-2"></div>
-              <p className="text-center">BORROWED</p>
-              <p className="text-center">MISSING</p>
+
+            <div className="p-2 rounded-[35px]">
+              <BookDisplaySkeleton className="w-full p-0 m-0"/>
+
+              <div className="px-5 gap-2 py-2 pb-5 text-xs flex flex-wrap">
+                <p className="px-4 py-1 bg-orange-700 rounded-full">BORROWED</p>
+                <p className="px-4 py-1 bg-red-800 rounded-full">MISSING</p>
+                <p className="px-4 py-1 bg-green-800 rounded-full">RETURNED</p>
+              </div>
             </div>
+            <div className="p-2 rounded-[35px]">
+              <BookDisplaySkeleton className="w-full p-0 m-0"/>
+
+              <div className="px-5 gap-2 py-2 pb-5 text-xs flex flex-wrap">
+                <p className="px-4 py-1 bg-orange-700 rounded-full">BORROWED</p>
+                <p className="px-4 py-1 bg-red-800 rounded-full">MISSING</p>
+                <p className="px-4 py-1 bg-green-800 rounded-full">RETURNED</p>
+              </div>
+            </div>
+
+            <div className="p-2 rounded-[35px]">
+              <BookDisplaySkeleton className="w-full p-0 m-0"/>
+
+              <div className="px-5 gap-2 py-2 pb-5 text-xs flex flex-wrap">
+                <p className="px-4 py-1 bg-orange-700 rounded-full">BORROWED</p>
+                <p className="px-4 py-1 bg-red-800 rounded-full">MISSING</p>
+                <p className="px-4 py-1 bg-green-800 rounded-full">RETURNED</p>
+              </div>
+            </div>
+
+            <div className="p-2 rounded-[35px]">
+              <BookDisplaySkeleton className="w-full p-0 m-0"/>
+
+              <div className="px-5 gap-2 py-2 pb-5 text-xs flex flex-wrap">
+                <p className="px-4 py-1 bg-orange-700 rounded-full">BORROWED</p>
+                <p className="px-4 py-1 bg-red-800 rounded-full">MISSING</p>
+                <p className="px-4 py-1 bg-green-800 rounded-full">RETURNED</p>
+              </div>
+            </div>
+
           </div>
+
         </div>
       </div>
     </div>
