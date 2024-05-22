2514eefae5adef358451f13193b266ff6a65644c
Isaac Estrella <isaacestrella12@gmail.com>
2024-05-22 11:23:22 -0400

    Fixed the admin page searching

--- app/src/subpages/SearchStudentAdminPage.tsx
+++ app/src/subpages/SearchStudentAdminPage.tsx
@@ -1,5 +1,5 @@
 import { Input } from "@/components/ui/input";
-import { ScrollArea } from "@/components/ui/scroll-area"
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 import { BorrowBookDisplay } from "@/components/BorrowBookDisplay";
 import { useState } from "react";
@@ -13,6 +13,7 @@ export const SearchStudentAdminPage = () => {
   const username = userDataFromSearch?.userMetaData[0].user_name;
   const borrows = userDataFromSearch?.borrrows;
   const userStatistics = userDataFromSearch?.borrowStats;
+  const showSearchResults = userDataFromSearch && !isLoading;
 
   return (
     <div className="w-[80%] p-4 font-outfit">
@@ -24,32 +25,42 @@ export const SearchStudentAdminPage = () => {
           placeholder="Search user by their email"
           className="w-full px-4 py-2 mb-4 rounded "
         />
-        <div className="text-white">
-          <div className="my-[40px] p-2">
-            <h2 className="text-3xl mb-5">
-              Name: {isLoading ? "loading" : username}
-            </h2>
+        {showSearchResults && (
+          <div className="text-white">
+            <div className="my-[40px] p-2">
+              <h2 className="text-3xl mb-5">
+                Name: {isLoading ? "loading" : username}
+              </h2>
 
-            <div className="flex text-lg text-gray-700">
-              <p className="p-1 px-4 mx-1 rounded-full bg-white">
-                Borrowed: <span className="font-bold text-black">{userStatistics?.borrowed}</span>
-              </p>
-              <p className="p-1 px-4 mx-1 rounded-full bg-white text-black">
-                Returned: <span className="font-bold">{userStatistics?.returned}</span>
-              </p>
-              <p className="p-1 px-4 mx-1 rounded-full bg-orange-700 text-white">
-                Missing: <span className="font-bold">{userStatistics?.missing}</span>
-              </p>
+              <div className="flex text-lg text-gray-700">
+                <p className="p-1 px-4 mx-1 rounded-full bg-white">
+                  Borrowed:{" "}
+                  <span className="font-bold text-black">
+                    {userStatistics?.borrowed}
+                  </span>
+                </p>
+                <p className="p-1 px-4 mx-1 rounded-full bg-white text-black">
+                  Returned:{" "}
+                  <span className="font-bold">{userStatistics?.returned}</span>
+                </p>
+                <p className="p-1 px-4 mx-1 rounded-full bg-orange-700 text-white">
+                  Missing:{" "}
+                  <span className="font-bold">{userStatistics?.missing}</span>
+                </p>
+              </div>
             </div>
+
+            {!isLoading ? (
+              <ScrollArea className="bg-transparent h-[55vh]">
+                {borrows?.map((borrow: Borrow, key: number) => {
+                  return <BorrowBookDisplay bookData={borrow} key={key} />;
+                })}
+              </ScrollArea>
+            ) : (
+              <></>
+            )}
           </div>
-
-          {!isLoading ? <ScrollArea className="bg-transparent h-[55vh]">
-            {borrows?.map((borrow: Borrow, key: number) => {
-              return <BorrowBookDisplay bookData={borrow} key={key}/>
-            })}
-          </ScrollArea> : <></>}
-
-        </div>
+        )}
       </div>
     </div>
   );
