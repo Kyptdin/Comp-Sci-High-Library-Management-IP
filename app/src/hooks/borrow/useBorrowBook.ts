c01b8cd9aa46929b1c3b100e1eac774c5ab7c6ca
Isaac Estrella <isaacestrella12@gmail.com>
2024-05-23 17:51:07 -0400

    Added the error and success for the borrowing hook

--- app/src/hooks/borrow/useBorrowBook.ts
+++ app/src/hooks/borrow/useBorrowBook.ts
@@ -1,9 +1,26 @@
 import { useMutation } from "@tanstack/react-query";
 import { borrowBook } from "@/services/bookService";
+import { useToast } from "@/components/ui/use-toast";
 
 export const useBorrowBook = () => {
+  const { toast } = useToast();
+
   const mutation = useMutation({
     mutationFn: borrowBook,
+    onSuccess: () => {
+      toast({
+        variant: "default",
+        title: "Successfully Borrowed Book",
+        description: "You have successfully borrowed the book.",
+      });
+    },
+    onError: (error) => {
+      toast({
+        variant: "destructive",
+        title: "Failed to Borrow Book",
+        description: error.message + ".",
+      });
+    },
   });
 
   return mutation;
