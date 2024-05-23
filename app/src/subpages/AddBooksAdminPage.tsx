5f0a3d18737c69d3d86f19b0e0fae76d13d284a7
Md Safi Momen <38478828+SafiMomen@users.noreply.github.com>
2024-05-23 12:55:27 -0400

    Validates the ISBN + frontend

--- app/src/subpages/AddBooksAdminPage.tsx
+++ app/src/subpages/AddBooksAdminPage.tsx
@@ -7,15 +7,21 @@ import { Separator } from "@/components/ui/separator";
 import { useState } from "react";
 import { FaFileUpload } from "react-icons/fa";
 import { FaInfoCircle } from "react-icons/fa";
-import { useCreateBook } from "@/hooks/book/useCreateBook";
+import { FaSquareCheck } from "react-icons/fa6";
+import { FaSquareXmark } from "react-icons/fa6";
 
 import useSWRMutation from "swr/mutation";
+import { useCreateBook } from "@/hooks/book/useCreateBook";
 import { isbnApiLink, fetchBookFromIsbn } from "@/utils/isbnApi";
+import { useBookValidator } from "@/hooks/book/useBookValidator";
 
 export const AddBooksAdminPage = () => {
   const { mutateAsync: createBook } = useCreateBook();
+
   const [bookIsbn, setBookIsbn] = useState<string>("");
   const [totalCopies, setTotalCopies] = useState<number>(0);
+  const { bookValidated } = useBookValidator(bookIsbn);
+
   const { toast } = useToast();
   const { isMutating, trigger } = useSWRMutation(
     isbnApiLink,
@@ -69,18 +75,23 @@ export const AddBooksAdminPage = () => {
         onSubmit={onSubmit}
       >
         <h2>Enter book ISBN number</h2>
-        <Input
-          value={bookIsbn}
-          onChange={(e) => setBookIsbn(e.target.value)}
-          type="text"
-          placeholder="Book ISBN number"
-          required={true}
-          className={cn(
-            "focus:outline-none text-black",
-            "rounded-md w-full py-2 px-5",
-            "duration-50 transition ease-out"
-          )}
-        />
+        <div className="flex justify-start items-center">
+          <Input
+            value={bookIsbn}
+            onChange={(e) => setBookIsbn(e.target.value)}
+            type="text"
+            placeholder="Book ISBN number"
+            required={true}
+            className={cn(
+              "focus:outline-none text-black",
+              "rounded-md w-full py-2 px-5 mr-3",
+              "duration-50 transition ease-out"
+            )}
+          />
+          {bookValidated ? 
+            <FaSquareCheck size={28} className="text-green-600"/> : 
+            <FaSquareXmark size={28} className="text-red-600"/>}
+        </div>
         <p className="text-sm text-teal-600 flex">
           <FaInfoCircle size={20} className="mr-2" />
           The ISBN number of the book is located above the bar-code in the back
