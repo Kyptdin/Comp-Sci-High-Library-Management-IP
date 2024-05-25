5ada390edb8236be6a67630f4687f837e1bb582a
Isaac Estrella <isaacestrella12@gmail.com>
2024-05-24 22:53:25 -0400

    Added the edit hooks to the edit book subpage

--- app/src/components/EditBookDisplay.tsx
+++ app/src/components/EditBookDisplay.tsx
@@ -22,87 +22,101 @@ import { FaTrash } from "react-icons/fa";
 import { IoEyeSharp } from "react-icons/io5";
 import { useState } from "react";
 import { useEditBook } from "@/hooks/book/useEditBook";
+import { BooksUpdate } from "@/types/supabaseTypes";
 
-export const EditBookDisplay = ({ isbn }: { isbn: string }) => {
-  const { data, error, isLoading } = useSWR(getIsbnLink(isbn), fetcher);
+interface Props {
+  title: string;
+  isbn: string;
+  bookImage?: string;
+}
+
+export const EditBookDisplay = ({ isbn, title, bookImage }: Props) => {
   const { mutateAsync: editBook, isPending: isEditingBook } = useEditBook();
   const [amountOfCopiesInBook, setAmountOfCopiesInBook] = useState<number>();
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
 
-  const firstBookItem = data?.items[0];
-  const info = firstBookItem?.volumeInfo;
   const inspectPage = `/inspect/${isbn}`;
 
-  const onSubmit = () => {
-    // editBook({isbn: });
-    alert(amountOfCopiesInBook);
+  const onSubmit = async () => {
+    const udpateBookObj: BooksUpdate = {
+      total_copies_within_school: amountOfCopiesInBook,
+    };
+    await editBook({ isbn, newBookData: udpateBookObj });
   };
 
   return (
     <div className="justify-start flex mb-2">
-      {isLoading | error ? (
+      {/* {isLoading | error ? (
         <BookDisplaySkeleton className="p-0 mr-5 h-[220px] w-[150px] rounded-md" />
-      ) : (
-        <BookDisplayImage
-          src={info?.imageLinks?.smallThumbnail}
-          className="p-0 mr-5 h-[220px] w-[150px] rounded-md"
-        />
-      )}
+      ) : ( */}
+      <BookDisplayImage
+        src={bookImage}
+        className="p-0 mr-5 h-[220px] w-[150px] rounded-md"
+      />
+      {/* )} */}
 
-      {!isLoading ? (
-        <div className="py-7 flex flex-col justify-around">
-          <div className="text-white mb-5">
-            <h1 className="text-2xl">{info?.title}</h1>
-            <h2 className="text-white mb-2">ISBN: {isbn}</h2>
+      <div className="py-7 flex flex-col justify-around">
+        <div className="text-white mb-5">
+          <h1 className="text-2xl">{title}</h1>
+          <h2 className="text-white mb-2">ISBN: {isbn}</h2>
 
-            <div className="full-center justify-start gap-2 mt-7">
-              <Link to={inspectPage}>
-                <Button variant="secondary">
-                  <IoEyeSharp className="mr-2" size={16} />
-                  View
-                </Button>
-              </Link>
-
-              <Dialog>
-                <DialogTrigger asChild>
-                  <Button variant="secondary">
-                    <MdEdit className="mr-2" size={16} />
-                    Edit
-                  </Button>
-                </DialogTrigger>
-
-                <DialogContent>
-                  <DialogHeader>
-                    <DialogTitle>Edit book</DialogTitle>
-
-                    <div className="flex items-center pt-5">
-                      <h1 className="text-black mr-3">Amount</h1>
-                      <Input
-                        value={amountOfCopiesInBook}
-                        type="number"
-                        onChange={(e) =>
-                          setAmountOfCopiesInBook(parseInt(e.target.value))
-                        }
-                      />
-                    </div>
-                  </DialogHeader>
-                  <DialogFooter>
-                    <Button onClick={onSubmit} disabled={isEditingBook}>
-                      Confirm
-                    </Button>
-                  </DialogFooter>
-                </DialogContent>
-              </Dialog>
-
-              <Button variant="destructive" className="bg-red-700">
-                <FaTrash className="mr-2" size={16} />
-                Remove
+          <div className="full-center justify-start gap-2 mt-7">
+            <Link to={inspectPage}>
+              <Button variant="secondary">
+                <IoEyeSharp className="mr-2" size={16} />
+                View
               </Button>
-            </div>
+            </Link>
+
+            <Dialog
+              open={isDialogOpen}
+              onOpenChange={() => {
+                setAmountOfCopiesInBook(0);
+              }}
+            >
+              <DialogTrigger asChild>
+                <Button
+                  variant="secondary"
+                  onClick={() => {
+                    setIsDialogOpen(true);
+                  }}
+                >
+                  <MdEdit className="mr-2" size={16} />
+                  Edit
+                </Button>
+              </DialogTrigger>
+
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Edit book</DialogTitle>
+
+                  <div className="flex items-center pt-5">
+                    <h1 className="text-black mr-3">Amount</h1>
+                    <Input
+                      min={0}
+                      value={amountOfCopiesInBook}
+                      type="number"
+                      onChange={(e) =>
+                        setAmountOfCopiesInBook(parseInt(e.target.value))
+                      }
+                    />
+                  </div>
+                </DialogHeader>
+                <DialogFooter>
+                  <Button onClick={onSubmit} disabled={isEditingBook}>
+                    Confirm
+                  </Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+
+            <Button variant="destructive" className="bg-red-700">
+              <FaTrash className="mr-2" size={16} />
+              Remove
+            </Button>
           </div>
         </div>
-      ) : (
-        <></>
-      )}
+      </div>
     </div>
   );
 };
--- app/src/subpages/EditBooksAdminPage.tsx
+++ app/src/subpages/EditBooksAdminPage.tsx
@@ -2,8 +2,14 @@ import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 
 import { EditBookDisplay } from "@/components/EditBookDisplay";
+import { useSearchBookBySimilarTitle } from "@/hooks/book/useSearchBookBySimilarTitle";
+import { useState } from "react";
 
 export const EditBooksAdminPage = () => {
+  const [bookNameQuery, setBookNameQuery] = useState<string>("");
+  const { data: bookDataSearchedByTitle } =
+    useSearchBookBySimilarTitle(bookNameQuery);
+
   return (
     <div className="text-white w-[80%] p-4">
       <div className="p-4">
@@ -11,14 +17,27 @@ export const EditBooksAdminPage = () => {
         <Separator className="w-full bg-gray-500 my-5" />
 
         <Input
+          value={bookNameQuery}
+          onChange={(e) => {
+            setBookNameQuery(e.target.value);
+          }}
           type="text"
-          placeholder="Enter ISBN number"
+          placeholder="Enter book name"
           className="w-full px-4 py-2 mb-4 rounded text-lg text-black"
         />
       </div>
 
       <div className="px-4 grid grid-cols-2">
-        <EditBookDisplay isbn="0747549559" />
+        {bookDataSearchedByTitle &&
+          bookDataSearchedByTitle.map((bookData) => {
+            return (
+              <EditBookDisplay
+                isbn="1338596705"
+                title={bookData.title}
+                bookImage={bookData.googleBooksApiData.image}
+              />
+            );
+          })}
       </div>
     </div>
   );
