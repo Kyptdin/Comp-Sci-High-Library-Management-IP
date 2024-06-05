7000a2dc2736f8350c67ccb86c2913d31b5cff28
Isaac Estrella <isaacestrella12@gmail.com>
2024-06-04 20:54:30 -0400

    Fixed code to message students every day on 9 am

--- frontend/src/supabase/supabaseClient.ts
+++ frontend/src/supabase/supabaseClient.ts
@@ -9,7 +9,8 @@ import { Database } from "./schema";
 
 // Supabase URL and API key for authentication
 const supabaseUrl = "https://vygjxzhtqazwuskkaxpz.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9....";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5Z2p4emh0cWF6d3Vza2theHB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUwMTg2MjgsImV4cCI6MjAzMDU5NDYyOH0.7c2_7Hv_fcJchjmJ1amzNgAJVgrobMMOYKAZjB0uhIU";
 
 // Create and export the Supabase client
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
