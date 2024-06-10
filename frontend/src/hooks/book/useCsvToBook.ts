d9cd40b1ef44c17af9bab5c0723e2fabbb421265
Md Safi Momen <Momen10462@gmail.com>
2024-06-10 04:12:29 -0400

    Added Queuing, but bugs exist

---
+++ frontend/src/hooks/book/useCsvToBook.ts
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+
+import { dataInterface, ValidateBookEntry } from "@/types/csvBookInterface";
+import { VolumeList } from "@/types/googleBooksAPI";
+
+import { useCreateBook } from "@/hooks/book/useCreateBook";
+import { isbnApiLink, fetchBookFromIsbn } from "@/utils/isbnApi";
+
+export const useCsvToBook = (bookDataContainer: dataInterface[] | undefined) => {
+    const { mutateAsync: createBook } = useCreateBook();
+    const [booksValidated, setBooksValidated] = 
+        useState<ValidateBookEntry[]>([]);
+
+    const addValidatedBook = async (validatedBook: ValidateBookEntry) => {
+        setBooksValidated((oldValidatedBooks) => 
+            [...oldValidatedBooks, validatedBook]);
+
+        if (validatedBook.Validated) {
+            try {
+                await createBook({
+                    id: validatedBook.Isbn,
+                    total_copies_within_school: validatedBook.Copies,
+                    title: validatedBook.Title,
+                });
+            } catch {
+                console.log(`Book exists already "${validatedBook.Title}"`);
+            }
+        };
+    };
+
+    const validateBook = (bookData: dataInterface) => {
+        if (!bookData.ISBN || bookData.ISBN.length <= 0) 
+            return;
+
+        let validateBookEntry: ValidateBookEntry = {
+            Isbn: bookData.ISBN || "--",
+            Validated: false,
+            Title: "--",
+            Copies: bookData.COPIES
+        };
+        
+        fetchBookFromIsbn(isbnApiLink, {
+            arg: bookData.ISBN
+        }).then((bookDataFromGoogle: VolumeList) => {
+            if (bookDataFromGoogle.totalItems <= 0) {
+                addValidatedBook(validateBookEntry);
+                return;
+            }
+            
+            let { volumeInfo } = bookDataFromGoogle.items[0];
+            let { title } = volumeInfo;
+
+            validateBookEntry.Title = title;
+            validateBookEntry.Validated = true;
+            addValidatedBook(validateBookEntry);
+        });
+    };
+
+    useEffect(() => {
+        if (!bookDataContainer) return
+
+        // Split array into chunks using reduce method
+        let updatingChunks = [];
+        const chunkSize = 5;
+        for (let i = 0; i < bookDataContainer.length; i += chunkSize) {
+            updatingChunks.push(bookDataContainer.slice(i, i + chunkSize));
+        }
+
+        // validate the books using timeout
+        for (let i = 0; i < updatingChunks.length; i++) {
+            let updatingChunk = updatingChunks[i];
+            
+            setTimeout(() => {
+                updatingChunk.forEach((bookData: dataInterface) => {
+                    validateBook(bookData);
+                });
+            }, 2000 * i)
+        }
+    }, [bookDataContainer]);
+
+    return { booksValidated };
+};
--- frontend/src/subpages/MultiAddBooksAdminPage.tsx
+++ frontend/src/subpages/MultiAddBooksAdminPage.tsx
@@ -3,44 +3,51 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableHead,
-    TableHeader,
-    TableRow,
+  Table,
+  TableBody,
+  TableCaption,
+  TableHead,
+  TableHeader,
+  TableRow,
   } from "@/components/ui/table"
-
-import { useEffect, useState } from "react";
-import Papa from "papaparse";
-// import { useMassCreateBooks } from "@/hooks/book/useMassCreateBooks";
 import { BookAddTablePill } from "@/components/BookAddTablePill";
 
-import { dataInterface } from "@/types/csvBookInterface";
+import { useState } from "react";
+import Papa from "papaparse";
+import { useCsvToBook } from "@/hooks/book/useCsvToBook";
+import { ValidateBookEntry, dataInterface } from "@/types/csvBookInterface";
+import { useToast } from "@/components/ui/use-toast"
 
 export const MultiAddBooksAdminPage = () => {
-    const [csvFile, setCsvFile] = useState(undefined);
-    const [csvBookData, setCsvBookData] = useState<dataInterface[]>();
+  const [csvFile, setCsvFile] = useState(undefined);
+  const [csvBookData, setCsvBookData] = useState<dataInterface[]>();
+  const { booksValidated } = useCsvToBook(csvBookData);
+
+  const { toast } = useToast();
 
   const handleCsvUpload = (event: any) => {
     setCsvFile(event.target.files[0]);
   };
-  const uploadCsvUpload = (event: any) => {
+  const uploadCsvFile = (event: any) => {
     event.preventDefault();
 
     if (!csvFile) return;
 
-        Papa.parse(csvFile, {
-            header: true,
-            skipEmptyLines: true,
-            complete: function(results: any) {
-                const { data }: {data: dataInterface[]} = results;
-                console.log(data);
-                setCsvBookData(data);
-            }
-        });
-    };
-    // const { mutate } = useMassCreateBooks();
+    Papa.parse(csvFile, {
+      header: true,
+      skipEmptyLines: true,
+      complete: function (results: any) {
+        const { data }: { data: dataInterface[] } = results;
+        setCsvBookData(data);
+      }
+    });
+
+    toast({
+      title: "WARNING: Uploading books",
+      description: "Don't close tab or upload new CSV",
+      variant: "destructive",
+    })
+  };
 
   return (
     <div className="p-4 w-[80%] font-outfit text-white full-center flex-col justify-start">
@@ -49,35 +56,39 @@ export const MultiAddBooksAdminPage = () => {
 
       <div className="flex p-4 gap-3">
         <Input type={"file"} accept={".csv"} onChange={handleCsvUpload}></Input>
-        <Button variant="secondary" onClick={uploadCsvUpload}>
+        <Button variant="secondary" onClick={uploadCsvFile}>
           Confirm CSV
         </Button>
       </div>
-      <h1 className="mb-5">{csvFile ? csvFile?.name : "Upload a file"}</h1>
+      
+      <h1 className="mb-5">{csvFile ? csvFile['name'] : "Upload a file"}</h1>
 
       <Separator className="w-1/2 bg-gray-700" />
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-1/4 text-white">ISBN</TableHead>
+            <TableHead className="w-1/4 text-white">Title</TableHead>
+            <TableHead className="w-1/4 text-white">Copies</TableHead>
+            <TableHead className="w-1/4 text-right text-white">ISBN Found</TableHead>
+          </TableRow>
+        </TableHeader>
 
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead className="w-1/4 text-white">ISBN</TableHead>
-                        <TableHead className="w-1/4 text-white">Title</TableHead>
-                        <TableHead className="w-1/4 text-white">Copies</TableHead>
-                        <TableHead className="w-1/4 text-right text-white">ISBN Found</TableHead>
-                    </TableRow>
-                </TableHeader>
-            </Table>
+      </Table>
 
-            <Table>
-                <TableCaption>A list of uploaded books.</TableCaption>
-                <TableBody>
-                    <ScrollArea className="bg-transparent h-[55vh] w-full">
-                        {csvBookData?.map((bookData : dataInterface, key: number) => {
-                            return <BookAddTablePill bookData={bookData} key={key}/>
-                        })}
-                    </ScrollArea>
-                </TableBody>
-            </Table>
-        </div>
-    );
+      <ScrollArea className="bg-transparent h-[55vh] w-full">
+        <Table>
+          <TableBody>
+              {booksValidated?.map((bookData: ValidateBookEntry, key: number) => {
+                return <BookAddTablePill bookData={bookData} key={key} />
+              })}
+          </TableBody>
+        </Table>
+      </ScrollArea>
+
+      <Table>
+        <TableCaption>A list of uploaded books.</TableCaption>
+      </Table>
+    </div>
+  );
 };
